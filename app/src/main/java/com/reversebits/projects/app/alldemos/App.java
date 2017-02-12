ee675336aa1c977d2c1a816fdf63755384d9ad58
TapanHP <rajparmar9395@gmail.com>
2017-02-12 23:21:32 +0530

    Demo done

--- app/src/main/java/com/reversebits/projects/app/alldemos/App.java
+++ app/src/main/java/com/reversebits/projects/app/alldemos/App.java
@@ -1,9 +1,34 @@
 package com.reversebits.projects.app.alldemos;
 
 import android.app.Application;
+import android.content.Context;
 
 
 public class App extends Application {
+
+    public App(){}
+
+
+    private static Context context; //See the warning
+
+    public static Context getContext() {
+        return context;
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        context = base;
+        instance = this;
+    }
+
+    //Using singleton pattern to get application context
+    private static App instance;
+
+    public static App getInstance() {
+        return instance;
+    }
+
 
 }
 
